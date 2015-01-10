02867c7438fdc0660ec695fa4eeec86a519dd8ab
David Bain <david@alteroo.com>
2015-01-09 22:35:35 -0500

    added views.py

---
+++ src/collective/fullcalendarioview/views.py
@@ -0,0 +1,26 @@
+import csv
+import os
+import random
+import shutil
+import string
+from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile as \
+    render_template
+from zope.i18nmessageid import MessageFactory
+
+
+_ = MessageFactory("johnpatrick.theme")
+
+
+class CalendarView(BrowserView):
+    """@@ep-result-view"""
+    template = render_template('calendar.pt')
+
+    def hello(self):
+        return "world"
+
+    def __call__(self):
+        self.nothingyet = "Nothing Yet"
+        # available in the template
+        # as view/summary
+        return self.template()
